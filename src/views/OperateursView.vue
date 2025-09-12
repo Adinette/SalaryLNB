89eec7873b30e78177727c96c7149feacc5a7afa
Adinette <108472474+Adinette@users.noreply.github.com>
2025-09-12 15:21:24 +0100

    Update OperateursView.vue

--- src/views/OperateursView.vue
+++ src/views/OperateursView.vue
@@ -15,6 +15,9 @@
 
     <!-- Tableau -->
     <div class="shadow-md rounded-2xl">
+		<div v-if="!operators.length === 0">
+					<h4 class="text-lg font-medium text-gray-900 mb-4 lg:mb-0 text-center lg:text-start">Aucun opérateurs ajouté</h4>
+		</div>
       <div class="relative overflow-x-auto md:overflow-hidden shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead class="text-base bg-green-600 text-white uppercase">

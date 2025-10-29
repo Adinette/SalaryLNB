b740e9525c98633a03ebbb5b7d43144abd2ef94d
Adinette <adinettedougloui@gmail.com>
2025-10-29 06:29:19 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,17 @@
-// main.ts
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { registerPlugins } from '../src/plugins/2.router/index'
+import { routes } from 'vue-router/auto-routes'
 
-// Template components
-import BaseBlock from "../src/components/BaseBlock.vue";
-import BaseBackground from "../src/components/BaseBackground.vue";
-import BasePageHeading from "../src/components/BasePageHeading.vue";
+const app = createApp(App)
 
-// Template directives
-import clickRipple from "../src/directives/clickRipple";
+registerPlugins(app)
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
 
-// Bootstrap framework
-import * as bootstrap from "bootstrap";
-window.bootstrap = bootstrap;
-// Global CSS
-// import "../src/assets/scss/oneui.min.css"
-import "../src/assets/tailwind.css";
-import "vue-toastification/dist/index.css";
-
-// Utils
-import { AppUtils } from "./utils";
-
-// ⚡ Import du router configuré
-import router from "./router";
-
-const app = createApp(App);
-
-// Global components
-app.component("BaseBlock", BaseBlock);
-app.component("BaseBackground", BaseBackground);
-app.component("BasePageHeading", BasePageHeading);
-
-// Register global directives
-app.directive("click-ripple", clickRipple);
-
-// Register plugins
-AppUtils.registerPlugins(app);
-
-// ⚡ Brancher le router
-app.use(router);
-
-// Mount app
-app.mount("#app");
+app.use(router)
+app.mount('#app')

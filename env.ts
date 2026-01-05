c03b01a93718e19ea85b78ed82367804c7744069
Adinette <adinettedougloui@gmail.com>
2026-01-05 11:50:01 +0100

    fix

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import {  EnvEnum } from "./src/enums/index";
 
 export const appEnv = {
   [EnvEnum.PRODUCTION]: {
-    apiUrl: "http://localhost:3000",
+    apiUrl: "https://salarylnb-backend.onrender.com/",
     // Ajoutez d'autres variables d'environnement si nécessaire
   },
   [EnvEnum.LOCAL]: {
